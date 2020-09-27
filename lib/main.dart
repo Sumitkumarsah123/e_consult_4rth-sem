7f4e98ef14e62a58bc0ee817af22b0aa3aa540a3
Sumit <sahsumit769@gmail.com>
2020-09-27 16:30:09 +0545

    changed notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:demo3/setup/category.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+//import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
 
 void main() {
+  // var initializationSettingsAndroid =
+  //     AndroidInitializationSettings('codex_logo');
+  // var initializationSettingsIOS = IOSInitializationSettings(
+  //     requestAlertPermission: true,
+  //     requestBadgePermission: true,
+  //     requestSoundPermission: true,
+  //     onDidReceiveLocalNotification:
+  //         (int id, String title, String body, String payload) async {});
+  // var initializationSettings = InitializationSettings(
+  //     initializationSettingsAndroid, initializationSettingsIOS);
+  // await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+  //     onSelectNotification: (String payload) async {
+  //   if (payload != null) {
+  //     debugPrint('notification payload: ' + payload);
+  //   }
+  // });
+
   runApp(MaterialApp(
     home: MyApp(),
   ));
--- lib/setup/home.dart
+++ lib/setup/home.dart
@@ -5,6 +5,9 @@ import 'package:demo3/setup/patient/screens/HomePage.dart';
 import 'package:demo3/setup/patient/screens/ChatPage.dart';
 import 'package:demo3/setup/patient/screens/ProfilePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+//import '../main.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -72,6 +75,41 @@ class _MyMainPageState extends State<MyHomePage> {
         selectedItemColor: Colors.white,
         onTap: _onItemTapped,
       ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: () {
+      //     print('pressed');
+      //     scheduleAlarm();
+      //   },
+      //   label: Text('Reminder'),
+      //   icon: Icon(Icons.notifications),
+      // ),
     );
   }
 }
+
+// void scheduleAlarm() async {
+//   var scheduledNotificationDateTime = DateTime.now().add(Duration(seconds: 5));
+
+//   var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+//     'alarm_notif',
+//     'alarm_notif',
+//     'Channel for Alarm notification',
+//     icon: 'codex_logo',
+//     sound: RawResourceAndroidNotificationSound('swiftly'),
+//     largeIcon: DrawableResourceAndroidBitmap('codex_logo'),
+//   );
+
+//   var iOSPlatformChannelSpecifics = IOSNotificationDetails(
+//       sound: 'swiftly.mp3',
+//       presentAlert: true,
+//       presentBadge: true,
+//       presentSound: true);
+//   var platformChannelSpecifics = NotificationDetails(
+//       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+//   await flutterLocalNotificationsPlugin.schedule(
+//       0,
+//       'eConsult',
+//       'Hey! Check the latest updates about COVID-19',
+//       scheduledNotificationDateTime,
+//       platformChannelSpecifics);
+// }
